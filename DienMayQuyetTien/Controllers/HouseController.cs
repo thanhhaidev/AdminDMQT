6e7789e703ca6e51c18e234d81316d801ccd5e8e
NguyenDinhSongHiep <hiep51149@gmail.com>
2018-12-17 10:28:36 +0700

    add frontend

---
+++ DienMayQuyetTien/Controllers/HouseController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DienMayQuyetTien.Controllers
+{
+    public class HouseController : Controller
+    {
+        // GET: House
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GioiThieu()
+        {
+            return View();
+        }
+
+        public ActionResult LienHe()
+        {
+            return View();
+        }
+
+        public ActionResult KhuyenMai()
+        {
+            return View();
+        }
+
+        public ActionResult TinTuc()
+        {
+            return View();
+        }
+    }
+}
