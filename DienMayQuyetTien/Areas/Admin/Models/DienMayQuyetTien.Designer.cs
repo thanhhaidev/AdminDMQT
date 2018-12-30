4f330fcd7f6843c4777c79ec56138527200cd5c6
thanghuynhm <thanghuynhm@gmail.com>
2018-12-30 10:59:03 +0700

    update fontend news

--- DienMayQuyetTien/Areas/Admin/Models/DienMayQuyetTien.Designer.cs
+++ DienMayQuyetTien/Areas/Admin/Models/DienMayQuyetTien.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'E:\AdminDMQT\DienMayQuyetTien\Areas\Admin\Models\DienMayQuyetTien.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\ThangHuynh\AdminDMQT\DienMayQuyetTien\Areas\Admin\Models\DienMayQuyetTien.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
