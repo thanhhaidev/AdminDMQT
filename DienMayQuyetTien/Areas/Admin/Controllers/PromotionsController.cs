20552b08e7ff32df3ade8fd81bc1b8c0f99b53ee
NguyenDinhSongHiep <hiep51149@gmail.com>
2018-12-30 10:38:56 +0700

    Delete Contacts

--- DienMayQuyetTien/Areas/Admin/Controllers/PromotionsController.cs
+++ DienMayQuyetTien/Areas/Admin/Controllers/PromotionsController.cs
@@ -7,6 +7,9 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using DienMayQuyetTien.Areas.Admin.Models;
+using System.IO;
+using System.Text;
+using System.Transactions;
 
 namespace DienMayQuyetTien.Areas.Admin.Controllers
 {
@@ -17,7 +20,7 @@ namespace DienMayQuyetTien.Areas.Admin.Controllers
         // GET: Admin/Promotions
         public ActionResult Index()
         {
-            //return View(db.Promotions.ToList());
+            
             if (Session["UserName"] != null)
             {
                 ViewBag.Message = TempData["message"];
@@ -49,7 +52,7 @@ namespace DienMayQuyetTien.Areas.Admin.Controllers
         {
             if (Session["UserName"] != null)
             {
-                ViewBag.ProductTypeID = new SelectList(db.ProductTypes, "ID", "ProductTypeName");
+                ViewBag.PromotionID = new SelectList(db.Promotions, "ID", "Name");
                 return View();
             }
             else
@@ -63,31 +66,73 @@ namespace DienMayQuyetTien.Areas.Admin.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ID,Name,Image,Start,Stop,Content,Status")] Promotion promotion)
+        
+
+        public ActionResult Create(Promotion promotion)
         {
+            //CheckValidationPromotion(promotion);
             if (ModelState.IsValid)
             {
-                db.Promotions.Add(promotion);
-                db.SaveChanges();
-                return RedirectToAction("Index");
+                using (var scope = new TransactionScope())
+                {
+                    if (Request.Files["ImageFile"] != null && Request.Files["ImageFile"].ContentLength < 2097152)
+                    {
+                        //string fileName = Path.GetFileNameWithoutExtension(product.ImageFile.FileName);
+                        string extension = Path.GetExtension(Request.Files["ImageFile"].FileName);
+                        string fileName = RandomString(5, true) + DateTime.Now.ToString("yymmssfff") + extension;
+                        promotion.Image = "/Assets/Admin/img/promotions/" + fileName;
+                        fileName = Path.Combine(Server.MapPath("/Assets/Admin/img/promotions/"), fileName);
+                        Request.Files["ImageFile"].SaveAs(fileName);
+                        db.Promotions.Add(promotion);
+                        db.SaveChanges();
+                        scope.Complete();
+                        TempData["message"] = "Tạo khuyễn mãi thành công.";
+                        return RedirectToAction("Index");
+                    }
+                    else
+                        ModelState.AddModelError("Image", "Chưa có hình khuyến mãi hoặc hình ảnh lớn hơn 2MB!");
+                }
             }
-
+            ViewBag.PromotionID = new SelectList(db.Promotions, "ID", "Name", promotion.ID);
             return View(promotion);
         }
+
+        private string RandomString(int size, bool lowerCase)
+        {
+            StringBuilder sb = new StringBuilder();
+            char c;
+            Random rand = new Random();
+            for (int i = 0; i < size; i++)
+            {
+                c = Convert.ToChar(Convert.ToInt32(rand.Next(65, 87)));
+                sb.Append(c);
+            }
+            if (lowerCase)
+                return sb.ToString().ToLower();
+            return sb.ToString();
+
+        }
+
+        //private void CheckValidationPromotion(Promotion model)
+        //{
+        //    if (model.Name == null || model.Name.Equals("") || model.Name.StartsWith(" ") || model.Name.EndsWith(" "))
+        //        ModelState.AddModelError("Name", "Name không được bỏ trống hoặc khoảng trống hoặc khoảng trống");
+
+        //}
 
         // GET: Admin/Promotions/Edit/5
         public ActionResult Edit(int? id)
         {
-            if (id == null)
+            if (Session["UserName"] != null)
             {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                Promotion promotion = db.Promotions.Find(id);
+                ViewBag.Promotion = db.Promotions.OrderByDescending(x => x.ID).ToList();
+                return View(promotion);
             }
-            Promotion promotion = db.Promotions.Find(id);
-            if (promotion == null)
+            else
             {
-                return HttpNotFound();
+                return RedirectToAction("Login", "Login");
             }
-            return View(promotion);
         }
 
         // POST: Admin/Promotions/Edit/5
@@ -95,14 +140,37 @@ namespace DienMayQuyetTien.Areas.Admin.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,Name,Image,Start,Stop,Content,Status")] Promotion promotion)
+        
+
+        public ActionResult Edit(Promotion promotion)
         {
+            //CheckValidationPromotion(promotion);
             if (ModelState.IsValid)
             {
-                db.Entry(promotion).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
+                using (var scope = new TransactionScope())
+                {
+                    if (Request.Files["ImageFile"] != null && Request.Files["ImageFile"].ContentLength < 2097152)
+                    {
+                        var fileNameOld = Server.MapPath(promotion.Image);
+                        if (System.IO.File.Exists(fileNameOld))
+                        {
+                            System.IO.File.Delete(fileNameOld);
+                        }
+                        //string fileName = Path.GetFileNameWithoutExtension(product.ImageFile.FileName);
+                        string extension = Path.GetExtension(Request.Files["ImageFile"].FileName);
+                        string fileName = RandomString(5, true) + DateTime.Now.ToString("yymmssfff") + extension;
+                        promotion.Image = "/Assets/Admin/img/promotions/" + fileName;
+                        fileName = Path.Combine(Server.MapPath("/Assets/Admin/img/promotions/"), fileName);
+                        Request.Files["ImageFile"].SaveAs(fileName);
+                    }
+                    db.Entry(promotion).State = EntityState.Modified;
+                    db.SaveChanges();
+                    scope.Complete();
+                    TempData["message"] = "Chỉnh sửa sản phẩm thành công.";
+                    return RedirectToAction("Index");
+                }
             }
+            ViewBag.Promotion = new SelectList(db.Promotions, "ID", "Name", promotion.ID);
             return View(promotion);
         }
 
@@ -126,10 +194,15 @@ namespace DienMayQuyetTien.Areas.Admin.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
-            Promotion promotion = db.Promotions.Find(id);
-            db.Promotions.Remove(promotion);
-            db.SaveChanges();
-            return RedirectToAction("Index");
+                Product product = db.Products.Find(id);
+                db.Products.Remove(product);
+
+                
+
+                db.SaveChanges();
+                TempData["message"] = "Xóa sản phẩm thành công.";
+                return RedirectToAction("Index");
+            
         }
 
         protected override void Dispose(bool disposing)
