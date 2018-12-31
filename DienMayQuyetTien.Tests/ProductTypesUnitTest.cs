f8ea4d25fd807b74a364038572182478499e20ca
NguyenDinhSongHiep <hiep51149@gmail.com>
2018-12-31 16:06:44 +0700

    Test Index ProductTypes

--- DienMayQuyetTien.Tests/ProductTypesUnitTest.cs
+++ DienMayQuyetTien.Tests/ProductTypesUnitTest.cs
@@ -13,9 +13,26 @@ using DienMayQuyetTien.Areas.Admin.Controllers;
 
 namespace DienMayQuyetTien.Tests
 {
-    [TestClass]
-    public class ProductTypesUnitTest
+    [TestMethod]
+    public void IndexTest()
     {
-       
+        var controller = new ProductTypesController();
+        var context = new Mock<HttpContextBase>();
+        var session = new Mock<HttpSessionStateBase>();
+        context.Setup(c => c.Session).Returns(session.Object);
+        controller.ControllerContext = new ControllerContext(context.Object, new RouteData(), controller);
+        session.Setup(s => s["UserName"]).Returns("abc");
+
+        var result = controller.Index() as ViewResult;
+        var db = new DmQT03Entities();
+
+        //Assert.IsNotNull(result.ViewBag.Message);
+        Assert.IsInstanceOfType(result.Model, typeof(List<ProductType>));
+        Assert.AreEqual(db.Products.Count(), (result.Model as List<ProductType>).Count);
+
+        session.Setup(s => s["UserName"]).Returns(null);
+        var redirect = controller.Index() as RedirectToRouteResult;
+        Assert.AreEqual("Login", redirect.RouteValues["action"]);
+        Assert.AreEqual("Login", redirect.RouteValues["controller"]);
     }
 }
