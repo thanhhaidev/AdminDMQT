fa0c91b52d29e8cb5976e0a49c6e2a91be992734
Thanh Hai Dev <hai2571998@gmail.com>
2018-12-31 16:02:32 +0700

    build v1.0.1 edit01

---
+++ DienMayQuyetTien.Tests/ProductTypesUnitTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using DienMayQuyetTien.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+using DienMayQuyetTien.Areas.Admin.Models;
+using System.Collections.Generic;
+using Moq;
+using System.Web;
+using System.Web.Routing;
+using System.Transactions;
+using DienMayQuyetTien.Areas.Admin.Controllers;
+
+namespace DienMayQuyetTien.Tests
+{
+    [TestClass]
+    public class ProductTypesUnitTest
+    {
+       
+    }
+}
